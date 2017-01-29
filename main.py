dd3e2c8416e9a7fe92ede1d6829dfc71168aa633
joel <joeltburton@gmail.com>
2017-01-29 17:22:23 -0600

    made formBuilder() function to handle assembling of the user signup page. also built the form to handle user inputs.

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
+import cgi
+
+
+def formBuilder(input=''):
+    ''' This function handles building the html webpage that contains the 
+        user signup form.
+    '''
+
+    header = "<h1>User Signup</h1>"
+    form = '''
+    <table>
+        <tr>
+            <td>Username: </td>
+            <td><input type="text" name="username"/></td>
+        </tr>
+        <tr>
+            <td>Passord: </td>
+            <td><input type="text" name="password"></input></td>
+        </tr>
+        <tr>
+            <td> Confirm Password: </td>
+            <td><input type="text" name="pw_match"></input></td>
+        </tr>
+        <tr>
+            <td>E-mail (optional): </td>
+            <td><input type="text" name="email"></input></td>
+        </tr>
+    </table>
+    '''
+
+
+    content = header + form
+    return content
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write(formBuilder())
+
+
+
+
+
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
