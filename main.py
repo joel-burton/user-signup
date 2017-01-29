f704b6ae9cdafc98cee9a569c9a74ee91e991f56
joel <joeltburton@gmail.com>
2017-01-29 17:34:25 -0600

    updated types of form inputs to mask password, etc. Added sign up button, and post method to form.

--- main.py
+++ main.py
@@ -4,30 +4,33 @@ import cgi
 
 
 def formBuilder(input=''):
-    ''' This function handles building the html webpage that contains the 
+    ''' This function handles building the html webpage that contains the
         user signup form.
     '''
 
     header = "<h1>User Signup</h1>"
     form = '''
-    <table>
-        <tr>
-            <td>Username: </td>
-            <td><input type="text" name="username"/></td>
-        </tr>
-        <tr>
-            <td>Passord: </td>
-            <td><input type="text" name="password"></input></td>
-        </tr>
-        <tr>
-            <td> Confirm Password: </td>
-            <td><input type="text" name="pw_match"></input></td>
-        </tr>
-        <tr>
-            <td>E-mail (optional): </td>
-            <td><input type="text" name="email"></input></td>
-        </tr>
-    </table>
+    <form method="post">
+        <table>
+            <tr>
+                <td>Username: </td>
+                <td><input type="text" name="username"/></td>
+            </tr>
+            <tr>
+                <td>Passord: </td>
+                <td><input type="password" name="password"></input></td>
+            </tr>
+            <tr>
+                <td> Confirm Password: </td>
+                <td><input type="password" name="pw_match"></input></td>
+            </tr>
+            <tr>
+                <td>E-mail (optional): </td>
+                <td><input type="email" name="email"></input></td>
+            </tr>
+        </table>
+        <input type="submit" value="Sign Up!">
+    </form>
     '''
 
 
@@ -36,8 +39,14 @@ def formBuilder(input=''):
 
 
 class MainHandler(webapp2.RequestHandler):
+
     def get(self):
         self.response.write(formBuilder())
+
+    def post(self):
+        self.response.write(formBuilder())
+
+
 
 
 
