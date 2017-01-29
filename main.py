1fce1ad6953bee7c0ee065dc734d3599d0cc0cf0
joel <joeltburton@gmail.com>
2017-01-29 17:54:47 -0600

    updated app to remember user inputs from form.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import webapp2
 import cgi
 
 
-def formBuilder(input=''):
+def formBuilder(un='', pw='', pwm='', em=''):
     ''' This function handles building the html webpage that contains the
         user signup form.
     '''
@@ -14,24 +14,24 @@ def formBuilder(input=''):
         <table>
             <tr>
                 <td>Username: </td>
-                <td><input type="text" name="username"/></td>
+                <td><input type="text" name="username"/ value={username}></td>
             </tr>
             <tr>
                 <td>Passord: </td>
-                <td><input type="password" name="password"></input></td>
+                <td><input type="password" name="password" value={password}></input></td>
             </tr>
             <tr>
                 <td> Confirm Password: </td>
-                <td><input type="password" name="pw_match"></input></td>
+                <td><input type="password" name="pw_match" value={pw_match}></input></td>
             </tr>
             <tr>
                 <td>E-mail (optional): </td>
-                <td><input type="email" name="email"></input></td>
+                <td><input type="email" name="email" value={email}></input></td>
             </tr>
         </table>
         <input type="submit" value="Sign Up!">
     </form>
-    '''
+    '''.format(username=un, password=pw, pw_match=pwm, email=em)
 
 
     content = header + form
@@ -44,7 +44,18 @@ class MainHandler(webapp2.RequestHandler):
         self.response.write(formBuilder())
 
     def post(self):
-        self.response.write(formBuilder())
+        username = self.request.get("username")
+        password = self.request.get("password")
+        pw_match = self.request.get("pw_match")
+        email = self.request.get("email")
+
+
+
+
+        self.response.write(formBuilder(username, password, pw_match, email))
+
+
+
 
 
 
