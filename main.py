969e11a6e620532fc0b06f6a66a41e3f3611c02b
joel <joeltburton@gmail.com>
2017-01-29 20:24:18 -0600

    add error messages and functions to handle user inputs, error handling.

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 
 import webapp2
 import cgi
+import re
 
 
-def formBuilder(un='', pw='', pwm='', em=''):
+
+username_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+password_RE = re.compile(r"^.{3,20}$")
+email_RE    = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+
+def usernameValid(username):
+    return username_RE.match(username)
+
+
+def passwordValid(password):
+    return password_RE.match(password)
+
+
+def matcher(var1, var2):
+    # intention for use to check that password entries match
+    return var1 == var2
+
+
+def emailValid(address):
+    return email_RE.match(address)
+
+
+
+
+
+def formBuilder(username="", password="", pw_match="", email="",
+                username_error="", password_error="",
+                pw_match_error="", email_error=""):
     ''' This function handles building the html webpage that contains the
         user signup form.
     '''
+
 
     header = "<h1>User Signup</h1>"
     form = '''
@@ -15,23 +44,36 @@ def formBuilder(un='', pw='', pwm='', em=''):
             <tr>
                 <td>Username: </td>
                 <td><input type="text" name="username"/ value={username}></td>
+                <td class="error">{un_invalid}</td>
             </tr>
             <tr>
-                <td>Passord: </td>
+                <td>Password: </td>
                 <td><input type="password" name="password" value={password}></input></td>
+                <td class="error">{pw_invalid}</td>
             </tr>
             <tr>
                 <td> Confirm Password: </td>
                 <td><input type="password" name="pw_match" value={pw_match}></input></td>
+                <td class="error">{not_matching}</td>
             </tr>
             <tr>
                 <td>E-mail (optional): </td>
                 <td><input type="email" name="email" value={email}></input></td>
+                <td class ="error">{em_invalid}</td>
             </tr>
         </table>
         <input type="submit" value="Sign Up!">
     </form>
-    '''.format(username=un, password=pw, pw_match=pwm, email=em)
+    '''.format(
+    username = cgi.escape(username, quote=True),
+    password = cgi.escape(password, quote=True),
+    pw_match = cgi.escape(pw_match, quote=True),
+    email = cgi.escape(email, quote=True),
+    un_invalid = username_error,
+    pw_invalid = password_error,
+    not_matching = pw_match_error,
+    em_invalid = email_error,
+    )
 
 
     content = header + form
@@ -49,10 +91,43 @@ class MainHandler(webapp2.RequestHandler):
         pw_match = self.request.get("pw_match")
         email = self.request.get("email")
 
+        username_error = ""
+        password_error = ""
+        pw_match_error = ""
+        email_error    = ""
+
+        if len(username) == 0:
+            username_error = "Please enter a username."
+        elif not usernameValid(username):
+            username = ""
+            username_error = "That is not a valid username. Please try a new username."
+
+        if len(password) == 0:
+            password_error = "Please enter a password."
+        elif not passwordValid(password):
+            password_error = "That is not a valid password. Please enter a new password."
+        elif len(pw_match) == 0:
+            pw_match_error = "You didn't confirm your password. Please re-enter and confirm."
+        elif not matcher(password, pw_match):
+            pw_match_error = "Your passwords did not match. Please try again."
+
+        if email and not emailValid(email):
+            email_error = "That is not a vaild email address."
+
+        # reset the passwords to blank for security
+        password = ""
+        pw_match = ""
 
 
 
-        self.response.write(formBuilder(username, password, pw_match, email))
+
+
+
+
+
+        self.response.write(formBuilder(username, password, pw_match, email,
+                                        username_error, password_error,
+                                        pw_match_error, email_error))
 
 
 
